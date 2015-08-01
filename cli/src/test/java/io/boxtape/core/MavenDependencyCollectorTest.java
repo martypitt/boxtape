75451112ddf91ab94cd6ab187bba8f2237877d71
martin.pitt <martin.pitt@lab49.com>
2015-08-01 17:20:05 +0100

    Made test more robust

--- cli/src/test/java/io/boxtape/core/MavenDependencyCollectorTest.java
+++ cli/src/test/java/io/boxtape/core/MavenDependencyCollectorTest.java
@@ -2,9 +2,13 @@ package io.boxtape.core;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import io.boxtape.core.configuration.BoxtapeSettings;
 import io.boxtape.core.configuration.Configuration;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.cli.MavenCli;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,15 +22,29 @@ import static org.junit.Assert.assertThat;
 public class MavenDependencyCollectorTest {
 
     MavenDependencyCollector collector;
-    File sampleProjectHome = new File("./sample-project");
+    File sampleProjectHome;
     Project sampleProject;
     @Before
     public void setup() throws IOException {
+        setPathToSampleProject();
+
         MavenCli cli = new MavenCli();
         collector = new MavenDependencyCollector(cli);
-        assertThat("Sample project not found at " + sampleProjectHome.getCanonicalPath(), sampleProjectHome.exists(), is(true));
+
         sampleProject = new Project(sampleProjectHome.getCanonicalPath() , new Configuration(), new BoxtapeSettings());
 
+    }
+
+    /**
+     * We don't know where the test will run from (especially in certiain CI environments)
+     * so work out where sample-project is.
+     */
+    private void setPathToSampleProject() throws IOException {
+        ArrayList<String> executionLocation = Lists.newArrayList(StringUtils.split(new File(".").getCanonicalPath(), "/"));
+        List<String> pathsToProject = executionLocation.subList(0, executionLocation.indexOf("boxtape") + 1);
+        pathsToProject.add("sample-project");
+        sampleProjectHome = new File("/" + Joiner.on("/").join(pathsToProject));
+        assertThat("Sample project not found at " + sampleProjectHome.getCanonicalPath(), sampleProjectHome.exists(), is(true));
     }
 
     @Test
